96b93adb0cb89c9bc048d5fd306c9fe86ddad251
Rusutikaa <jpdzl.tqu@gmail.com>
2021-05-16 23:28:17 +0900

    2021-5-10

--- docs/developer.apple.com2/tutorials/js/footer.js
+++ docs/developer.apple.com2/tutorials/js/footer.js
@@ -1,11 +1,3 @@
-const languageSelector = document.getElementById('footer-language-selector');
-if (languageSelector) {
-  languageSelector.onchange = (event) => {
-    const { value } = event.target;
-    window.location = value;
-  };
-}
-
 document.querySelectorAll('#directorynav h3').forEach((item) => {
   item.addEventListener('click', (event) => {
     item.classList.toggle('enhance');
